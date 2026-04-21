7ffbd6d04a5241b976523ca9b019bf5d8564d989
IsabellaJimenez21 <A01709505@tec.mx>
2026-04-21 01:00:45 -0600

    Update ProgCounter.vhd
    ProggCounter Completo

--- ProgCounter.vhd
+++ ProgCounter.vhd
@@ -28,4 +28,18 @@ architecture Behavioral of ProgCounter is
 
 begin
 
+    process(Clk, Rst)
+    begin
+        if Rst = '1' then
+            -- Al resetear, el PC vuelve a la dirección inicial que es 0
+            PCOut <= (others => '0');
+        elsif rising_edge(Clk) then
+            -- Solo se actualiza si el Clock Enable (Cen) está activo
+				-- Libera el numero nuevo, que vuelve a entrar para convertirce en el numero actual
+            if Cen = '1' then
+                PCOut <= PCIn;
+            end if;
+        end if;
+    end process;
+
 end Behavioral;
