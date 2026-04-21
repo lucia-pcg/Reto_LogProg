0892b5d8dcdc2fb1a9c64855d70ce1b0192db170
lucia-pcg <A01713181@tec.mx>
2026-04-21 09:04:17 -0600

    Refactor Mux4to1 entity and add selection logic
    Updated entity and architecture for Mux4to1 with selection process.

--- Mux4to1.vhd
+++ Mux4to1.vhd
@@ -1,41 +1,38 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: ITESM
+-- Engineer: Lucía Casas
 -- 
--- Create Date:    17:20:32 01/27/2009 
--- Design Name: 
--- Module Name:    Mux4to1 - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
+-- Create Date:    16:15:00 04/20/2026
+-- Design Name:	 Mux4to1.vhd
+-- Module Name:    Mux4to1 - Behavioral
+-- Project Name: 	 Reto Microprocesador
 -- Description: C09, C10: 8-bit 4 to 1 multiplexors used by the JiJr
 --              jumps and the data value for Registers
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Mux4to1 is
-  port(
-    InA   : in  STD_LOGIC_VECTOR (7 downto 0);
-	 InB   : in  STD_LOGIC_VECTOR (7 downto 0);
-	 InC   : in  STD_LOGIC_VECTOR (7 downto 0);
-	 InD   : in  STD_LOGIC_VECTOR (7 downto 0);
-	 Sel   : in  STD_LOGIC_VECTOR (1 downto 0);
-	 M     : out STD_LOGIC_VECTOR (7 downto 0));
+    port (
+        InA : in  STD_LOGIC_VECTOR (7 downto 0);
+        InB : in  STD_LOGIC_VECTOR (7 downto 0);
+        InC : in  STD_LOGIC_VECTOR (7 downto 0);
+        InD : in  STD_LOGIC_VECTOR (7 downto 0);
+        Sel : in  STD_LOGIC_VECTOR (1 downto 0); -- Señal de selección de 2 bits
+        M   : out STD_LOGIC_VECTOR (7 downto 0)  -- Salida del dato seleccionado
+    );
 end Mux4to1;
 
 architecture Behavioral of Mux4to1 is
-
 begin
-		  
+    process(InA, InB, InC, InD, Sel)
+    begin
+        case Sel is
+            when "00" => M <= InA;
+            when "01" => M <= InB;
+            when "10" => M <= InC;
+            when "11" => M <= InD;
+            when others => M <= (others => '0');
+        end case;
+    end process;
 end Behavioral;
- 
