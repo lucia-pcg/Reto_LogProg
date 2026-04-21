4ffb21921220d4875c19fd3a82990ffc51219bb4
lucia-pcg <A01713181@tec.mx>
2026-04-21 09:04:54 -0600

    Update entity and architecture of BrEq module

--- BrEq.vhd
+++ BrEq.vhd
@@ -1,38 +1,28 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: H.H.H. Embedded Systems Design Team
+-- Company: ITESM
+-- Engineer: Lucía Casas
 -- 
--- Create Date:    16:31:07 01/27/2009 
--- Design Name: 
+-- Create Date:    16:20:00 04/20/2026
+-- Design Name: 	 BrEq
 -- Module Name:    BrEq - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
+-- Project Name: 	 Reto Microprocesador
 -- Description: C08: Beq Instruction Multiplexor
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity BrEq is
-  port(
-    InA   : in  STD_LOGIC;
-	 InB   : in  STD_LOGIC;
-	 Sel   : in  STD_LOGIC;
-	 M     : out STD_LOGIC);
+    port (
+        InA : in  STD_LOGIC; -- Conectado a PCInc
+        InB : in  STD_LOGIC; -- Conectado a ALUZero
+        Sel : in  STD_LOGIC; -- Conectado a Beq (Selección de instrucción)
+        M   : out STD_LOGIC  -- Salida BrEqM (Controla si se suma el inmediato al PC)
+    );
 end BrEq;
 
 architecture Behavioral of BrEq is
-
 begin
-		  
+    -- Si Sel (Beq) es '1', la decisión depende de si son iguales (InB)
+    -- Si Sel es '0', sigue el flujo de incremento normal (InA)
+    M <= InB when (Sel = '1') else InA;
 end Behavioral;
-
