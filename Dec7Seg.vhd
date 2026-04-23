cef7241dc354847b96152c479c5151d90dbef62e
04diana <dspaisano@gmail.com>
2026-04-23 00:11:11 -0600

    Edit Dec7Seg

--- Dec7Seg.vhd
+++ Dec7Seg.vhd
@@ -27,6 +27,27 @@ end Dec7Seg;
 architecture Behavioral of Dec7Seg is
 
 begin
+	process(InR)
+	begin
+		case InR is 
+		
+		when "0000" => Seg <= "10000000"; --0
+		when "0001" => Seg <= "1111001";
+		when "0010" => Seg <= "0100100";
+		when "0011" => Seg <= "0110000";
+		when "0100" => Seg <= "0011001";
+		when "0101" => Seg <= "0010010";
+		when "0110" => Seg <= "0000010";
+		when "0111" => Seg <= "1111000";
+		when "1000" => Seg <= "0000000";
+		when "1001" => Seg <= "0010000";
+		when "1010" => Seg <= "0001000";
+		when "1011" => Seg <= "0000011";
+		when "1100" => Seg <= "1000110";
+		when "1101" => Seg <= "0100001";
+		when "1110" => Seg <= "0000110";
+		when "1111" => Seg <= "0001110";
+		when others => Seg <= "1111111";
 
 end Behavioral;
 
