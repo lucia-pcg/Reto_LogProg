010a359139c1096019ea2a6ac696cf00f60647ca
lucia-pcg <A01713181@tec.mx>
2026-04-24 09:55:26 -0600

    Refactor Dec7Seg module with updated output values
    Updated the Dec7Seg module to include a more detailed comment structure and modified the output values for the seven-segment display.

--- Dec7Seg.vhd
+++ Dec7Seg.vhd
@@ -2,52 +2,47 @@
 -- Company: ITESM - Campus Qro.
 -- Engineer: H.H.H. Embedded Systems Design Team
 -- 
--- Create Date:    16:29:16 01/24/2012 
+-- Create Date:    16:29:16 04/23/2026 
 -- Design Name: 
--- Module Name:    Top - Behavioral 
+-- Module Name:    Dec7Seg - Behavioral 
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
 -- Description: Bin to Seven Segment Decoder
 ----------------------------------------------------------------------------------
 library IEEE;
-
-library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Dec7Seg is
-  --C13 and C14: Hex to 7 seg decoder
   port (
     InR    : in  STD_LOGIC_VECTOR (3 downto 0);
-	 Seg    : out STD_LOGIC_VECTOR (7 downto 0));
+    Seg    : out STD_LOGIC_VECTOR (7 downto 0) -- 8 bits: DP, g, f, e, d, c, b, a
+  );
 end Dec7Seg;
 
 architecture Behavioral of Dec7Seg is
-
 begin
-	process(InR)
-	begin
-		case InR is 
-		
-		when "0000" => Seg <= "10000000"; --0
-		when "0001" => Seg <= "1111001";
-		when "0010" => Seg <= "0100100";
-		when "0011" => Seg <= "0110000";
-		when "0100" => Seg <= "0011001";
-		when "0101" => Seg <= "0010010";
-		when "0110" => Seg <= "0000010";
-		when "0111" => Seg <= "1111000";
-		when "1000" => Seg <= "0000000";
-		when "1001" => Seg <= "0010000";
-		when "1010" => Seg <= "0001000";
-		when "1011" => Seg <= "0000011";
-		when "1100" => Seg <= "1000110";
-		when "1101" => Seg <= "0100001";
-		when "1110" => Seg <= "0000110";
-		when "1111" => Seg <= "0001110";
-		when others => Seg <= "1111111";
-
+    process(InR)
+    begin
+        case InR is 
+            -- Formato: "DP g f e d c b a" (0 = encendido, 1 = apagado)
+            when "0000" => Seg <= "11000000"; -- 0 (DP off, g off, otros on)
+            when "0001" => Seg <= "11111001"; -- 1
+            when "0010" => Seg <= "10100100"; -- 2
+            when "0011" => Seg <= "10110000"; -- 3
+            when "0100" => Seg <= "10011001"; -- 4
+            when "0101" => Seg <= "10010010"; -- 5
+            when "0110" => Seg <= "10000010"; -- 6
+            when "0111" => Seg <= "11111000"; -- 7
+            when "1000" => Seg <= "10000000"; -- 8
+            when "1001" => Seg <= "10010000"; -- 9
+            when "1010" => Seg <= "10001000"; -- A
+            when "1011" => Seg <= "10000011"; -- b
+            when "1100" => Seg <= "11000110"; -- C
+            when "1101" => Seg <= "10100001"; -- d
+            when "1110" => Seg <= "10000110"; -- E
+            when "1111" => Seg <= "10001110"; -- F
+            when others => Seg <= "11111111"; -- Todo apagado
+        end case;    
+    end process;
 end Behavioral;
-
