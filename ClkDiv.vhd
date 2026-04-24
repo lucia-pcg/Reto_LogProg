11bdc092a93477c66ce1ab9e593ba0ac0cfa8579
Valeria Escalante <139800698+ValeriaEE@users.noreply.github.com>
2026-04-23 18:24:50 -0600

    Update ClkDiv.vhd
    Added frequency divider logic with reset functionality.

--- ClkDiv.vhd
+++ ClkDiv.vhd
@@ -18,21 +18,45 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity ClkDiv is
   generic (
-    Fout : natural);
+    Fout : natural := 5);
   port (
     Clkin  : in  STD_LOGIC;
     Rst    : in  STD_LOGIC;
-    Clkout : out STD_LOGIC);
+    Clkout : out STD_LOGIC
+  );
 end ClkDiv;
 
 architecture Behavioral of ClkDiv is
 
-begin
+ 
+  constant Fin : natural := 50000000;
   
+  constant MaxCount : natural := Fin / Fout;
+  
+  signal count : natural range 0 to MaxCount := 0;
+
+begin
+
+  process(Clkin, Rst)
+  begin
+    if Rst = '1' then
+      count <= 0;
+      Clkout <= '0';
+      
+    elsif rising_edge(Clkin) then
+      
+      if count = MaxCount - 1 then
+        count <= 0;       
+        Clkout <= '1';    
+      else
+        count <= count + 1; 
+        Clkout <= '0';      
+      end if;
+      
+    end if;
+  end process;
+
 end Behavioral;
-
-
-
 
 
 
