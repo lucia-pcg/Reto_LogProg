c448cfde571972bb1e6f7547586d74a8dc61524b
Valeria Escalante <139800698+ValeriaEE@users.noreply.github.com>
2026-04-23 18:14:30 -0600

    Update DispOff.vhd
    The architecture now sets the Turnoff output to turn off unused displays.

--- DispOff.vhd
+++ DispOff.vhd
@@ -11,19 +11,22 @@
 -- Description: Turn unused Displays off
 ----------------------------------------------------------------------------------
 library IEEE;
-library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity DispOff is
   port (
-    Turnoff: out STD_LOGIC_VECTOR (31 downto 0));
+    Turnoff: out STD_LOGIC_VECTOR (31 downto 0)
+  );
 end DispOff;
 
 architecture Behavioral of DispOff is
 
 begin
+  -- Al usar (others => '1'), le decimos al compilador que llene
+  -- los 32 bits del vector con 1 para que los 4 displays extra estén apagados.
+  Turnoff <= (others => '1');
 
 end Behavioral;
 
